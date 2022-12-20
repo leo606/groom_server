c91a8f66c5547b2bb78b88a4e7414046135d1b5f
Leo Ferreira <leonardoferreira@protonmail.com>
2022-12-19 23:32:37 -0300

    groom service

--- Services/GroomService.cs
+++ Services/GroomService.cs
@@ -16,7 +16,7 @@ public class GroomService : Groom.GroomBase
   public override async Task<RoomRegistrationResponse> RoomRegistration(RoomRegistrationRequest request, ServerCallContext context)
   {
     UsersQueues.CreateUserQueue(request.RoomName, request.UserName);
-    var resp = new RoomRegistrationResponse(){Joined=true};
+    var resp = new RoomRegistrationResponse() { Joined = true };
     return await Task.FromResult(resp);
   }
 
@@ -43,5 +43,84 @@ public class GroomService : Groom.GroomBase
       }
       await Task.Delay(500);
     }
+  }
+
+  public override async Task StartChat(IAsyncStreamReader<ChatMessage> incomingStream, IServerStreamWriter<ChatMessage> outgoingStream, ServerCallContext context)
+  {
+
+    // Wait for the first message to get the user name
+    while (!await incomingStream.MoveNext())
+    {
+      await Task.Delay(100);
+    }
+
+    string userName = incomingStream.Current.User;
+    string room = incomingStream.Current.Room;
+    Console.WriteLine($"User {userName} connected to room {incomingStream.Current.Room}");
+
+    // TEST
+    UsersQueues.CreateUserQueue(room, userName);
+    // END TEST END TEST END TEST
+
+    // Get messages from the user
+    var reqTask = Task.Run(async () =>
+    {
+      while (await incomingStream.MoveNext())
+      {
+        Console.WriteLine($"Message received: {incomingStream.Current.Contents}");
+        UsersQueues.AddMessageToRoom(ConvertToReceivedMessage(incomingStream.Current), incomingStream.Current.Room);
+      }
+    });
+
+
+    // Check for messages to send to the user
+    var respTask = Task.Run(async () =>
+    {
+      while (true)
+      {
+        var userMsg = UsersQueues.GetMessageForUser(userName);
+        if (userMsg != null)
+        {
+          var userMessage = ConvertToChatMessage(userMsg, room);
+          await outgoingStream.WriteAsync(userMessage);
+        }
+        if (MessagesQueue.GetMessagesCount() > 0)
+        {
+          var news = MessagesQueue.GetNextMessage();
+          var newsMessage = ConvertToChatMessage(news, room);
+          await outgoingStream.WriteAsync(newsMessage);
+        }
+
+        await Task.Delay(200);
+      }
+    });
+
+    // Keep the method running
+    while (true)
+    {
+      await Task.Delay(10000);
+    }
+  }
+
+  private ReceivedMessage ConvertToReceivedMessage(ChatMessage chatMsg)
+  {
+    var rcMsg = new ReceivedMessage();
+    rcMsg.Contents = chatMsg.Content;
+    rcMsg.MsgTime = chatMsg.MsgTime;
+    rcMsg.User = chatMsg.User;
+
+    return rcMsg;
+  }
+
+  private ChatMessage ConvertToChatMessage(ReceivedMessage rcMsg, string room)
+  {
+    var chatMsg = new ChatMessage();
+    chatMsg.Content = rcMsg.Contents;
+    chatMsg.MsgTime = rcMsg.MsgTime;
+    chatMsg.User = rcMsg.User;
+    chatMsg.Room = room;
+
+    return chatMsg
+
   }
 }
